7c38a164ce105a8f398f592ce49d91e803fcc28b
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2018-07-11 04:09:21 -0500

    Added Edit and Update functionality

--- config/routes.js
+++ config/routes.js
@@ -29,6 +29,8 @@ module.exports.routes = {
   '/articles/add': {view: 'add'},
   'POST /articles/create': 'ArticlesController.create',
   'POST /articles/delete/:id': 'ArticlesController.delete',
+  '/articles/edit/:id': 'ArticlesController.edit',
+  '/articles/update/:id': 'ArticlesController.update',
 
   /***************************************************************************
   *                                                                          *
